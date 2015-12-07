c28e0a90e3853a6c41d12a3da24bbbee9ad93b28
Matthew Lewis <malewis@esriuk.com>
2015-12-07 20:59:58 +0000

    Created an App

---
+++ JavaScriptExperiements/JavaScriptExperiements/Experiment_1/app.js
@@ -0,0 +1,14 @@
+﻿
+
+require(["dojo/dom-construct", "dojo/dom", "dojo/on","app/map"], function (domConstruct, dom, on, mapHandler) {
+
+    var appDiv = dom.byId("app")
+    var mapDiv = domConstruct.create("div", { id: "map" }, appDiv);
+    var browserBox = domConstruct.create("div", { id: "browse"}, appDiv)
+
+    console.log("Creating Div")
+    console.log(mapDiv.id)
+
+    mapHandler.loadMap()
+
+});
---
+++ JavaScriptExperiements/JavaScriptExperiements/Experiment_1/app/map.js
@@ -0,0 +1,21 @@
+﻿
+define(
+    ["esri/map", "dojo/dom"],
+    function (Map, dom) {
+
+    mapDiv = dom.byId("map")
+
+    function loadMap() {
+        map = new Map("map", {
+            basemap: "topo",  //For full list of pre-defined basemaps, navigate to http://arcg.is/1JVo6Wd
+            center: [-122.45, 37.75], // longitude, latitude
+            zoom: 13
+        });
+    }
+
+    return {
+        loadMap: loadMap,
+    }
+
+    }
+);
